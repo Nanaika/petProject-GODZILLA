49cf22ed48e645ec821b4c8b81b1c813b5dba9e3
Nanaika <zloyx2@mail.ru>
2020-10-28 21:23:59 +0500

    fixed some issues

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ import org.apache.commons.cli.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.Properties;
 
@@ -14,22 +15,30 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        System.out.println("Welcome to GODZILLA.\n" +
-                "Please enter command.\n" +
-                "or type help for list of commands.\n");
+        System.out.println("Welcome to GODZILLA.\n");
+
 
         Properties prop = new Properties();
         prop.load(new FileInputStream(new File("./src/config.ini")));
 
         Options options = new Options();
 
+
         Option parseOpt = new Option("p", "parse", true, "parse csv file");
         parseOpt.setRequired(false);
         options.addOption(parseOpt);
 
-        Option downloadOpt = new Option("d", "download", true, "download file");
+        Option downloadOpt = new Option("d", "download", true, "download csv file");
+
+        downloadOpt.setArgs(2);
+        downloadOpt.setValueSeparator(' ');
+
         downloadOpt.setRequired(false);
         options.addOption(downloadOpt);
+
+        Option backupOpt = new Option("b", "backup", true, "backup parsed data to file");
+        backupOpt.setRequired(false);
+        options.addOption(backupOpt);
 
 
         CommandLineParser clparser = new DefaultParser();
@@ -52,26 +61,55 @@ public class Main {
         if (cmd.hasOption("d")) {
 
             Downloader downloader = new Downloader();
-            downloader.setFileUrl(cmd.getOptionValue("d"));
-            downloader.setSaveDir(prop.getProperty("saveDir"));
+
+            String[] values = cmd.getOptionValues("d");
+
+            downloader.setFileUrl(values[0]);
+            downloader.setSaveDir(values[1]);
             downloader.downloadFile();
 
-        } else if (cmd.hasOption("p")) {
+        }
+        if (cmd.hasOption("p")) {
 
             Reader reader = new Reader(cmd.getOptionValue("p"));
             List<String[]> data = reader.readFile();
 
             Parser parser = new Parser(data);
             ParsedData parsedData = parser.calculateAllDeaths();
-
-            System.out.println("File parsed");
-            parsedData.backupData();
+            System.out.println("File parsed\n");
+            System.err.println("Deaths count is : " + parsedData.getAllDeaths() + "\n");
 
         }
+        if (cmd.hasOption("b")) {
+            if (!(cmd.hasOption("p"))){
+
+                System.out.println("Cant backup data without parsing file!");
+
+            }else {
+
+                Reader reader = new Reader(cmd.getOptionValue("p"));
+                List<String[]> data = reader.readFile();
+
+                Parser parser = new Parser(data);
+                ParsedData parsedData = parser.calculateAllDeaths();
+
+                parsedData.backupData();
+                System.out.println("Data backuped!");
+            }
+
+        }
+        if (cmd.getOptions().length == 0) {
+
+            System.err.println("     Possible commands : ");
+            PrintWriter pw = new PrintWriter(System.err);
+            formatter.printOptions(pw, 100, options, 10, 5);
+            pw.flush();
+            pw.close();
 
 
+        }
     }
-
+}
 
 //        boolean con = true;
 //
@@ -122,5 +160,5 @@ public class Main {
 //        }
 
 
-}
+
 
